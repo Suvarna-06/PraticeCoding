fa0ff3c0244bbce019e17d41d6d40b5598b7c010
Suvarna-06 <sandhyasankesh@gmail.com>
2025-03-28 12:28:21 +0530

    28th March 2025

---
+++ src/Java_Code_Programs/Prg8_string.java
@@ -0,0 +1,15 @@
+package Java_Code_Programs;
+
+public class Prg8_string {
+    public static void main(String[]args){
+        // 1. Without Paramter and without return type
+        greet();
+    }
+
+
+    static void greet(){
+        System.out.println("Hi i am sandhya");
+    }
+
+}
+
